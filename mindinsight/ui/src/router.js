a66f4ddd073ef87a0f6c2fb972720def974ca109
Jingli Xu <1220341948@qq.com>
2021-10-18 20:39:37 +0800

    feat: add marey graph Rounte

--- mindinsight/ui/src/router.js
+++ mindinsight/ui/src/router.js
@@ -87,6 +87,10 @@ export default new Router({
       component: () => import('./views/parallel-graph/graph-container.vue'),
     },
     {
+      path: '/marey-graph',
+      component: () => import('./views/marey-graph/marey-graph-container.vue'),
+    },
+    {
       path: '/profiling-gpu',
       component: () => import('./views/profiling-gpu/profiling.vue'),
       redirect: '/profiling-gpu/profiling-dashboard',
---
+++ mindinsight/ui/src/views/marey-graph/marey-graph-container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test"> </div>
+</template>
+
+<script>
+  console.log("Hello world")
+</script>
+
+<style>
+  .test{
+    height: 100%;
+  }
+</style>
