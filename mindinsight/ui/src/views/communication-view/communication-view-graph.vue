1f1ec3362bae61d9b08a0f8f773902d4da34c04e
LuJiaying <lujiaying@higgsasset.com>
2022-03-01 14:10:26 +0800

    feat: 矩形框选

--- mindinsight/ui/src/views/communication-view/communication-view-graph.vue
+++ mindinsight/ui/src/views/communication-view/communication-view-graph.vue
@@ -1,54 +1,92 @@
 <template>
   <div class="communication-view">
     <div>Communication View</div>
-    <svg id="communication-node-graph" style="width: 100%; height: 80%">
-      <defs>
-        <marker
-          id="arrow"
-          markerUnits="strokeWidth"
-          markerWidth="12"
-          markerHeight="12"
-          viewBox="0 0 12 12"
-          refX="6"
-          refY="6"
-          orient="auto"
-        >
-          <path d="M2,2 L10,6 L2,10 L6,6 L2,2" style="fill: #000000" />
-        </marker>
-      </defs>
-      <g ref="communication-graph-container" v-if="showSvgContainer">
-        <path
-          v-for="edge in linksData"
-          :key="edge.index"
-          :d="
-            createCPath(
-              edge.source.x,
-              edge.source.y,
-              edge.target.x,
-              edge.target.y
-            )
-          "
-          stroke="#0f0"
-          stroke-width="1.5px"
-          fill="none"
-          style="marker-end: url(#arrow)"
-        ></path>
-        <circle
-          v-for="node in nodesData"
-          :key="node.name"
-          :cx="node.x"
-          :cy="node.y"
-          :r="5"
-          fill="red"
-        ></circle>
-      </g>
-    </svg>
+    <div class="communication-graph-box" @mousedown="handleMouseDown">
+      <div
+        class="communication-graph-mask"
+        v-show="is_show_mask"
+        :style="
+          'width:' +
+          mask_width +
+          'left:' +
+          mask_left +
+          'height:' +
+          mask_height +
+          'top:' +
+          mask_top
+        "
+      ></div>
+      <svg id="communication-node-graph" style="width: 100%; height: 100%">
+        <defs>
+          <marker
+            id="arrow"
+            markerUnits="strokeWidth"
+            markerWidth="12"
+            markerHeight="12"
+            viewBox="0 0 12 12"
+            refX="6"
+            refY="6"
+            orient="auto"
+          >
+            <path d="M2,2 L10,6 L2,10 L6,6 L2,2" style="fill: #000000" />
+          </marker>
+        </defs>
+        <g ref="communication-graph-container" v-if="showSvgContainer">
+          <path
+            v-for="edge in linksData"
+            :key="edge.index"
+            :d="
+              createCPath(
+                edge.source.x,
+                edge.source.y,
+                edge.target.x,
+                edge.target.y
+              )
+            "
+            stroke="#0f0"
+            stroke-width="1.5px"
+            fill="none"
+            style="marker-end: url(#arrow)"
+          ></path>
+          <circle
+            v-for="node in nodesData"
+            :key="node.name"
+            :cx="node.x"
+            :cy="node.y"
+            :r="5"
+            fill="red"
+            class="communication-graph-circle"
+          ></circle>
+          <text
+            v-for="node in nodesData"
+            :key="'text' + node.name"
+            :x="node.x"
+            :y="node.y"
+          >
+            {{ node.name }}
+          </text>
+        </g>
+      </svg>
+    </div>
   </div>
 </template>
 
 <style>
-.communication-view circle {
-  z-index: 100;
+.communication-graph-box {
+  width: 100%;
+  height: 80%;
+  position: relative;
+  overflow: hidden;
+  user-select: none;
+}
+.communication-graph-mask {
+  position: absolute;
+  background: #409eff;
+  opacity: 0.4;
+  z-index: 999;
+}
+.communication-graph-box svg {
+  overflow: auto;
 }
 </style>
 <script>
@@ -68,16 +106,42 @@ export default {
       nodesData: {},
       linksData: {},
       showSvgContainer: true,
+      start_x: 0,
+      start_y: 0,
+      end_x: 0,
+      end_y: 0,
+      is_show_mask: false,
+      box_screen_left: 0,
+      box_screen_top: 0,
+      selectDevice: [],
     };
   },
   mounted() {
     this.initGraph();
   },
 
+  computed: {
+    mask_width() {
+      return `${Math.abs(this.end_x - this.start_x)}px;`;
+    },
+    mask_height() {
+      return `${Math.abs(this.end_y - this.start_y)}px;`;
+    },
+    mask_left() {
+      return `${Math.min(this.start_x, this.end_x) - this.box_screen_left}px;`;
+    },
+    mask_top() {
+      return `${Math.min(this.start_y, this.end_y) - this.box_screen_top}px;`;
+    },
+  },
+
   methods: {
     async initGraph() {
       await this.fetchData();
       this.generateGraph();
+      const dom_box = document.querySelector(".communication-graph-box");
+      this.box_screen_left = dom_box.getBoundingClientRect().left;
+      this.box_screen_top = dom_box.getBoundingClientRect().top;
     },
 
     async fetchData() {
@@ -120,6 +184,7 @@ export default {
     },
 
     generateGraph() {
+      this.selectDevice = [];
       let svgContainer = document.getElementById("communication-node-graph");
       let width = svgContainer.getBoundingClientRect().width;
       let height = svgContainer.getBoundingClientRect().height;
@@ -149,6 +214,7 @@ export default {
       this.nodesData = tmpnodesData;
 
       this.linksData = tmplinksData;
+      console.log(this.nodesData);
     },
 
     createCPath(x1, y1, x2, y2) {
@@ -156,24 +222,73 @@ export default {
         dy = y2 - y1,
         dr = Math.sqrt(dx * dx + dy * dy);
       if (dr == 0) {
-        return (
-          "M" +
-          (x1 - 1) +
-          "," +
-          (y1 - 1) +
-          "A" +
-          10 +
-          "," +
-          10 +
-          " 0 1 1 " +
-          x2 +
-          "," +
-          y2
-        );
+        x1 = x1 - 1;
+        y1 = y1 - 1;
+        dr = 20;
       }
       return (
         "M" + x1 + "," + y1 + "A" + dr + "," + dr + " 0 0 1 " + x2 + "," + y2
       );
+    },
+
+    handleMouseDown(event) {
+      if (event.target.tagName === "SPAN") return false;
+      this.is_show_mask = true;
+      this.start_x = event.clientX;
+      this.start_y = event.clientY;
+      this.end_x = event.clientX;
+      this.end_y = event.clientY;
+      // console.log(this.start_x);
+      document.body.addEventListener("mousemove", this.handleMouseMove);
+      document.body.addEventListener("mouseup", this.handleMouseUp);
+    },
+    handleMouseMove(event) {
+      this.end_x = event.clientX;
+      this.end_y = event.clientY;
+    },
+    handleMouseUp() {
+      document.body.removeEventListener("mousemove", this.handleMouseMove);
+      document.body.removeEventListener("mouseup", this.handleMouseUp);
+      // console.log(this.end_x);
+      this.is_show_mask = false;
+      this.handleDeviceSelect();
+      this.resSetXY();
+    },
+    resSetXY() {
+      this.start_x = 0;
+      this.start_y = 0;
+      this.end_x = 0;
+      this.end_y = 0;
+      this.selectDevice = [];
+    },
+    handleDeviceSelect() {
+      const dom_mask = window.document.querySelector(
+        ".communication-graph-mask"
+      );
+      const rect_select = dom_mask.getClientRects()[0];
+      document
+        .querySelectorAll(".communication-graph-circle")
+        .forEach((node, index) => {
+          const rects = node.getClientRects()[0];
+          if (this.collide(rects, rect_select) === true) {
+            // console.log(this.nodesData[index].name);
+            this.selectDevice.push(index);
+          }
+        });
+    },
+    collide(rect1, rect2) {
+      const maxX = Math.max(rect1.x + rect1.width, rect2.x + rect2.width);
+      const maxY = Math.max(rect1.y + rect1.height, rect2.y + rect2.height);
+      const minX = Math.min(rect1.x, rect2.x);
+      const minY = Math.min(rect1.y, rect2.y);
+      if (
+        maxX - minX <= rect1.width + rect2.width &&
+        maxY - minY <= rect1.height + rect2.height
+      ) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 };
