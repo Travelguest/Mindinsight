5460cd2161625461ceaf13e9f3e96734725c85ab
zerowangzy <756762961@qq.com>
2022-03-14 17:22:40 +0800

    feat: 同构子图外的圆圈

--- mindinsight/ui/src/js/profile-graph/force-layout.js
+++ mindinsight/ui/src/js/profile-graph/force-layout.js
@@ -65,4 +65,41 @@ export function layout(opNodes, normalEdges, nodeMap, tick = 200) {
       })
       .stop();
   sim.tick(tick);
+  // 将聚合节点保留一个单独显示的区域
+  const subGraphs = new Set();
+  for (const opNode of opNodes) {
+    if (opNode.isAggreNode) {
+      subGraphs.add(opNode.aggreNodes);
+    }
+  }
+
+  for (const subgraph of subGraphs) {
+    let right = -10000;
+    let left = 10000000;
+    for (const node of subgraph) {
+      if (node.x < left) left = node.x;
+      if (node.x > right) right = node.x;
+    }
+    const n = Number(subgraph[0].id.match(/\d+/g));
+    const nodeSet = new Set(subgraph);
+    let leftShift = 0;
+    let rightShift = 10000000;
+    for (const opNode of opNodes) {
+      if (Number(opNode.id.match(/\d+/g)) < n && !nodeSet.has(opNode)) {
+        if (opNode.x > leftShift) leftShift = opNode.x;
+      }
+      if (Number(opNode.id.match(/\d+/g)) > n && !nodeSet.has(opNode)) {
+        if (opNode.x < rightShift) rightShift = opNode.x;
+      }
+    }
+    const toRight1 = leftShift - left + 40;
+    const toRight2 = toRight1 - rightShift + right + 40;
+    for (const opNode of opNodes) {
+      if (nodeSet.has(opNode)) {
+        opNode.x += toRight1;
+      } else if (Number(opNode.id) > n) {
+        opNode.x += toRight2;
+      }
+    }
+  }
 }
